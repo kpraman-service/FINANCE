4292fcb753b650eae2d223740cfaeda3e159212d
Krishna Construction <janagiraman8586@gmail.com>
2026-08-24 23:34:13 +0530

    Add ambient module declaration for lucide-react

---
+++ frontend/types/declarations.d.ts
@@ -0,0 +1,22 @@
+declare module 'lucide-react' {
+  import React from 'react';
+  export const X: React.ComponentType<any>;
+  export const Download: React.ComponentType<any>;
+  export const FileText: React.ComponentType<any>;
+  export const TrendingUp: React.ComponentType<any>;
+  export const TrendingDown: React.ComponentType<any>;
+  export const Wallet: React.ComponentType<any>;
+  export const PiggyBank: React.ComponentType<any>;
+  export const Plus: React.ComponentType<any>;
+  export const ArrowUpRight: React.ComponentType<any>;
+  export const ArrowDownLeft: React.ComponentType<any>;
+  export const Calendar: React.ComponentType<any>;
+  export const Activity: React.ComponentType<any>;
+  export const LogOut: React.ComponentType<any>;
+  export const User: React.ComponentType<any>;
+  export const Shield: React.ComponentType<any>;
+  export const Target: React.ComponentType<any>;
+  export const Trash2: React.ComponentType<any>;
+  const content: any;
+  export default content;
+}
